e35f64a9621deb438a9390f80f22a3f46cf9f395
Omar Shaarawy <omarelshaarawy909@gmail.com>
2026-04-29 19:28:32 +0300

    Brochure_Creator_AI_App

--- src/config.py
+++ src/config.py
@@ -5,11 +5,9 @@ from dotenv import load_dotenv
 MODEL_1 = "gpt-5-nano"
 MODEL_2 = "gpt-4.1-mini"
 MODEL_3 = "gpt-4o-mini"
-FAV_ICON_URL = "assets/favicon.png"
+FAV_ICON_URL = "src/assets/favicon.png"
 
 def load_environment():
     load_dotenv(override=True)
     api_key = os.getenv('OPENAI_API_KEY')   
     return api_key
-
-
